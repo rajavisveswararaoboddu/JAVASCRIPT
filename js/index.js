aa91dd09857eeeec8f3135a65d6bbf27e1646ca1
rajavisveswararaoboddu <rajavisveswararaoboddu@gmail.com>
2024-12-24 17:49:05 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+//let username="visva";
+//let password="visva123";
+//let role="admin";
+//username == "visva" && password == "visva123" ?console.log("login succesful")  
+
+//let islogin = true;
+//let user = "admin";
+//islogin ? user == "customer" ? console.log("welcome to customer") : console.log("welcome to admin") : console .log("please login");
+
+
+
+let user = "vegitarian";
+let dish = "gobi";
+user == "vegitarian" ? dish == "gobi" ? console.log("panner") : console.log("gobi") : dish  == "chicken" ? console.log("chicken"): console.log("mutton");
